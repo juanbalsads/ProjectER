e847d734e6c0bb439a89f49afee1951778c0b477
Maria <Maria@10.60.90.44>
2019-04-24 17:14:35 +0200

    finished relationship with assosiation dosage!

--- src/ER/POJOS/Dosage.java
+++ src/ER/POJOS/Dosage.java
@@ -7,6 +7,10 @@ import javax.persistence.*;
 @IdClass(DosageId.class)
 
 public class Dosage implements Serializable  {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 777479542299658839L;
 	@Id
 	private Integer admission_id;
 	@Id
@@ -19,6 +23,67 @@ public class Dosage implements Serializable  {
 	@ManyToOne
 	@PrimaryKeyJoinColumn(name="drug_id", referencedColumnName="id")
 	private Drug drug;
+	public Integer getAdmission_id() {
+		return admission_id;
+	}
+	public void setAdmission_id(Integer admission_id) {
+		this.admission_id = admission_id;
+	}
+	public Integer getDrug_id() {
+		return drug_id;
+	}
+	public void setDrug_id(Integer drug_id) {
+		this.drug_id = drug_id;
+	}
+	public double getDosage() {
+		return dosage;
+	}
+	public void setDosage(double dosage) {
+		this.dosage = dosage;
+	}
+	public Admission getAdmission() {
+		return admission;
+	}
+	public void setAdmission(Admission admission) {
+		this.admission = admission;
+	}
+	public Drug getDrug() {
+		return drug;
+	}
+	public void setDrug(Drug drug) {
+		this.drug = drug;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((admission_id == null) ? 0 : admission_id.hashCode());
+		result = prime * result + ((drug_id == null) ? 0 : drug_id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dosage other = (Dosage) obj;
+		if (admission_id == null) {
+			if (other.admission_id != null)
+				return false;
+		} else if (!admission_id.equals(other.admission_id))
+			return false;
+		if (drug_id == null) {
+			if (other.drug_id != null)
+				return false;
+		} else if (!drug_id.equals(other.drug_id))
+			return false;
+		return true;
+	}
+	
+	
 	
 	
 	
--- src/ER/POJOS/DosageId.java
+++ src/ER/POJOS/DosageId.java
@@ -1,22 +1,46 @@
 package ER.POJOS;
 
 import java.io.Serializable;
-import javax.persistence.*;
 
 public class DosageId implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6599837575630304190L;
 	private Integer admission_id;
 	private Integer drug_id;
-	
-	
-	/*public int hashCode() {
-		return (int)(admissionId + drugId);
-		
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((admission_id == null) ? 0 : admission_id.hashCode());
+		result = prime * result + ((drug_id == null) ? 0 : drug_id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DosageId other = (DosageId) obj;
+		if (admission_id == null) {
+			if (other.admission_id != null)
+				return false;
+		} else if (!admission_id.equals(other.admission_id))
+			return false;
+		if (drug_id == null) {
+			if (other.drug_id != null)
+				return false;
+		} else if (!drug_id.equals(other.drug_id))
+			return false;
+		return true;
 	}
 	
-	public boolean equals(Object object) {
-		if(object instanceof DosageId)
-	    
-		   }*/
+	
+	
 
 
 }
