6a586e9cf070150d226d11e8b4b4a599b37d1cf1
juanbalsads <juan.balsa.ds@gmail.com>
2019-05-08 19:07:27 +0200

    juuuu

---
+++ src/GraficalUserInterface/Shooter.java
@@ -0,0 +1,12 @@
+package GraficalUserInterface;
+
+public class Shooter {
+
+	public static void main(String[] args) {
+		InitialWindow frame = new InitialWindow();
+		frame.setVisible(true);
+		
+
+	}
+
+}
