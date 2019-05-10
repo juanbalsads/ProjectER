0169e31258ca839bf2f98a67f7ec781075231965
juanbalsads <juan.balsa.ds@gmail.com>
2019-05-10 12:02:39 +0200

    GRafical interface

--- src/GraficalUserInterface/InitialWindow.java
+++ src/GraficalUserInterface/InitialWindow.java
@@ -12,66 +12,97 @@ import javax.swing.ImageIcon;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.SwingConstants;
+import javax.swing.ButtonGroup;
+import javax.swing.border.TitledBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class InitialWindow extends JFrame {
 
 	private JPanel contentPane;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 
 	public InitialWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(220, 100, 800, 500);
+		setBounds(220, 100, 820, 600);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(255, 255, 153));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setBounds(585, 114, 141, 212);
 		lblNewLabel_3.setIcon(new ImageIcon("C:\\Users\\juanb\\git\\ProjectER\\Images\\dataManager.png"));
-		lblNewLabel_3.setBounds(586, 124, 141, 212);
 		contentPane.add(lblNewLabel_3);
 		
 		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon("C:\\Users\\juanb\\git\\ProjectER\\Images\\nurse.png"));
 		label.setBounds(409, 124, 141, 212);
+		label.setIcon(new ImageIcon("C:\\Users\\juanb\\git\\ProjectER\\Images\\nurse.png"));
 		contentPane.add(label);
 		
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\juanb\\git\\ProjectER\\Images\\Doctor.png"));
 		lblNewLabel.setBounds(191, 114, 180, 212);
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\juanb\\git\\ProjectER\\Images\\Doctor.png"));
 		contentPane.add(lblNewLabel);
 		
 		JLabel label_1 = new JLabel("");
+		label_1.setBounds(20, 114, 180, 212);
 
 		label_1.setIcon(new ImageIcon("C:\\Users\\juanb\\git\\ProjectER\\Images\\Assistant.png"));
-		label_1.setBounds(20, 114, 180, 212);
 		contentPane.add(label_1);
 		
 		JLabel lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\juanb\\git\\ProjectER\\Images\\hospital100.png"));
 		lblNewLabel_1.setBounds(10, 0, 106, 128);
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\juanb\\git\\ProjectER\\Images\\hospital100.png"));
 		contentPane.add(lblNewLabel_1);
 		
-		JPanel nursePan = new JPanel();
-		nursePan.setBackground(new Color(255, 153, 0));
-		nursePan.setBounds(30, 344, 112, 39);
-		contentPane.add(nursePan);
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("ASSISTANT");
+		rdbtnNewRadioButton.setBounds(20, 333, 145, 32);
+		buttonGroup.add(rdbtnNewRadioButton);
+		rdbtnNewRadioButton.setFont(new Font("Gill Sans MT", Font.BOLD, 20));
+		rdbtnNewRadioButton.setBackground(new Color(255, 153, 0));
+		contentPane.add(rdbtnNewRadioButton);
 		
-		JLabel lblNurse = new JLabel("NURSE");
-		lblNurse.setFont(new Font("Gill Sans MT", Font.BOLD, 20));
-		nursePan.add(lblNurse);
-		nursePan.setVisible(false);
+		JRadioButton rdbtnDoctor = new JRadioButton("DOCTOR");
+		rdbtnDoctor.setBounds(214, 333, 132, 32);
+		buttonGroup.add(rdbtnDoctor);
+		rdbtnDoctor.setFont(new Font("Gill Sans MT", Font.BOLD, 20));
+		rdbtnDoctor.setBackground(new Color(255, 153, 0));
+		contentPane.add(rdbtnDoctor);
 		
-		label_1.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				nursePan.setVisible(true);
-				
+		JRadioButton rdbtnNurse = new JRadioButton("  NURSE");
+		rdbtnNurse.setBounds(419, 333, 131, 32);
+		buttonGroup.add(rdbtnNurse);
+		rdbtnNurse.setFont(new Font("Gill Sans MT", Font.BOLD, 20));
+		rdbtnNurse.setBackground(new Color(255, 153, 0));
+		contentPane.add(rdbtnNurse);
+		
+		JRadioButton rdbtnDbManager = new JRadioButton("DB MANAGER");
+		rdbtnDbManager.setBounds(585, 333, 171, 32);
+		buttonGroup.add(rdbtnDbManager);
+		rdbtnDbManager.setFont(new Font("Gill Sans MT", Font.BOLD, 20));
+		rdbtnDbManager.setBackground(new Color(255, 153, 0));
+		contentPane.add(rdbtnDbManager);
+		
+		JButton btnSignIn = new JButton("Sign In");
+		btnSignIn.setFont(new Font("Gill Sans MT", Font.BOLD, 14));
+		btnSignIn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {		
+				setVisible(false);
+				AssHW assHW = new AssHW();
+				assHW.setVisible(true);
 				
 			}
 		});
-
+		btnSignIn.setForeground(new Color(0, 0, 0));
+		btnSignIn.setBounds(499, 419, 106, 32);
+		btnSignIn.setBackground(new Color(255, 153, 51));
+		contentPane.add(btnSignIn);
+		
+		
 	}
-	
-	
 }
---
+++ src/GraficalUserInterface/ListAdm.java
@@ -0,0 +1,74 @@
+package GraficalUserInterface;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class ListAdm extends JFrame {
+
+	private JPanel contentPane;
+
+	public ListAdm() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(220, 100, 820, 600);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 153));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("  Admissions");
+		lblNewLabel.setFont(new Font("Gill Sans MT", Font.BOLD, 24));
+		lblNewLabel.setBounds(354, 36, 184, 48);
+		contentPane.add(lblNewLabel);
+		
+		
+		//RIGHT BUTT
+		
+		JButton btnendSession = new JButton("");
+		btnendSession.setIcon(new ImageIcon("C:\\Users\\juanb\\git\\ProjectER\\Images\\icons8-fin-de-cadena-24.png"));
+		btnendSession.setBounds(757, 11, 37, 38);
+		contentPane.add(btnendSession);
+		btnendSession.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				InitialWindow iniWini = new InitialWindow();
+				iniWini.setVisible(true);
+			}
+		});
+		
+		JButton btnHW = new JButton("");
+		btnHW.setIcon(new ImageIcon("C:\\Users\\juanb\\git\\ProjectER\\Images\\icons8-p\u00E1gina-principal-24.png"));
+		btnHW.setBounds(710, 11, 37, 38);
+		contentPane.add(btnHW);
+		btnHW.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AssHW assHW = new AssHW();
+				setVisible(false);
+				assHW.setVisible(true);
+			}
+		});
+
+		
+		JButton btnBack = new JButton("");
+		btnBack.setIcon(new ImageIcon("C:\\Users\\juanb\\git\\ProjectER\\Images\\icons8-izquierda-c\u00EDrculo-24 (1).png"));
+		btnBack.setBounds(663, 11, 37, 38);
+		contentPane.add(btnBack);
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+			}
+		});
+	}
+
+}
